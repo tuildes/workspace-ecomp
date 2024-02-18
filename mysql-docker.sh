c42efab5f3c4be88dd5a12ff87a3f490006f4d22
frehsegustavo <frehsegustavo@gmail.com>
2024-02-18 13:27:11 -0300

    update

--- mysql-docker.sh
+++
@@ -1,7 +0,0 @@
-docker run --name back \
- -e MARIADB_RANDOM_ROOT_PASSWORD=yes \
- -e MARIADB_USER=gfrehse \
- -e MARIADB_DATABASE=laravel \
- -e MARIADB_PASSWORD=12345 \
- -p 3306:3306 \
- -d mariadb:10.10
--- mysql.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update
-sudo apt install mysql-server
-sudo systemctl start mysql.service
